2efebe48e82cb6309596cc408f86934b58039b31
Ry <68780806+Ry-Projects@users.noreply.github.com>
2021-08-28 10:11:31 -0400

    Separate file for the words list, cleaned the code

--- src/app.js
+++ src/app.js
@@ -1,27 +1,16 @@
-const WORD_LIST = [
-    { word: "PIKACHU", hint: "Lightning Type" },
-    { word: "BATMAN", hint: "Nocturnal Cosplayer" },
-    { word: "MICHELANGELO", hint: "Turtle Painter" },
-];
-const scoreDiv = document.getElementById("score-area");
 const hintDiv = document.getElementById("hint-area");
 const wordDiv = document.getElementById("word-area");
 const letterDiv = document.getElementById("letter-btns");
+const livesSpan = document.getElementById("lives");
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWYXZ".split("");
 let lives = 5;
-let userStreak;
+let userStreak = 0;
 let word;
 let hiddenWord;
 
 document.addEventListener("load", initializeWord());
 
-
-scoreDiv.innerHTML = `<p>Lives Remaining: <span id="lives">5</span></p>
-                      <p>Current Streak: <span id="userStreak">0</span></p>
-                      <hr>`;
-
-for (let letter of letters) 
-    createLetterBtn(letter);
+for (let letter of letters) createLetterBtn(letter);
 const letterBtns = document.getElementsByClassName("letter-btn");
 for (let btn of letterBtns) {
     btn.addEventListener("click", guessLetter);
@@ -37,8 +26,7 @@ function createLetterBtn(letter) {
 function initializeWord() {
     let index = Math.floor(Math.random() * WORD_LIST.length);
     word = WORD_LIST[index].word;
-
-    // Initializing the blank spaces for the hidden word
+    // Creating the blank spaces for the hidden word
     hiddenWord = "";
     for (let i = 0; i < word.length; i++) {
         if (word[i] != " ") hiddenWord += "_";
@@ -51,7 +39,6 @@ function initializeWord() {
 function guessLetter(e) {
     let btn = e.target;
     let letter = btn.innerText;
-
     if (word.search(letter) >= 0) {
         for (let i = 0; i < word.length; i++) {
             if (word[i] === letter) {
@@ -74,8 +61,8 @@ function guessLetter(e) {
             console.log("Depressing loss :'(");
             // TODO - Losing Condition things ---------------------------------------------
             for (let btn of letterBtns) btn.classList.add("guessed");
-        } else document.getElementById("lives").innerText = lives;
+        } else livesSpan.innerText = lives;
     }
-
     btn.classList.add("guessed");
 }
+
---
+++ src/words.js
@@ -0,0 +1,7 @@
+const WORD_LIST = [
+    { word: "MICHELANGELO", hint: "Turtle Painter" },
+    { word: "PIKACHU", hint: "Electric Type" },
+    { word: "LIGHTNING BOLT", hint: "Wizard's Scar" },
+    { word: "BATMAN", hint: "Nocturnal Cosplayer" },
+    { word: "ALFRED HITCHCOCK", hint: "Famous Shadow" },
+];
